dcb80fa7930476b7842fcd635f4e4a4fe25d84a5
Jhonny <jhodeht@gmail.com>
2012-03-31 12:45:41 -0400

    Actualizado la interfaz de registro de clientes.

--- src/webroot/js/app/view/cliente/Add.js
+++ src/webroot/js/app/view/cliente/Add.js
@@ -5,7 +5,7 @@ Ext.define('SisInventarios.view.cliente.Add', {
     layout: 'fit',
     autoShow: true,
     modal:true,
-    width: 550,
+    width: 400,
     
     iconCls: 'icon-add',
     initComponent: function() {
@@ -20,58 +20,109 @@ Ext.define('SisInventarios.view.cliente.Add', {
                 name:'id',
                 xtype: 'hidden'
             },{
-                xtype:'textfield',
-                name:'nit_ci',
-                fieldLabel:'NIT o CI',
-                msgTarget:'side',
-                allowBlank:false,
-                vtype:'alphanum',
-                anchor:'95%'
+                xtype:'container',
+                layout: 'column',
+                style:{
+                    paddingBottom:'20px'
+                },
+                items:[{
+                    xtype:'container',
+                    columnWidth:.35,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'textfield',
+                        name:'nit_ci',
+                        fieldLabel:'NIT o CI',
+                        msgTarget:'side',
+                        allowBlank:false,
+                        vtype:'alphanum',
+                        anchor:'90%'
+                    }]
+                },{
+                    xtype:'container',
+                    columnWidth:.55,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'textfield',
+                        name:'nombres',
+                        fieldLabel:'Nombres',
+                        msgTarget:'side',
+                        allowBlank:false,
+                        vtype:'alpha',
+                        anchor:'90%'
+                    }]
+                }]                
             },{
-                xtype:'textfield',
-                name:'nombres',
-                fieldLabel:'Nombres',
-                msgTarget:'side',
-                allowBlank:false,
-                vtype:'alpha',
-                anchor:'95%'
+                xtype:'container',
+                layout: 'column',
+                style:{
+                    paddingBottom:'20px'
+                },
+                items:[{
+                    xtype:'container',
+                    columnWidth:.50,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'textfield',
+                        name:'apellido_paterno',
+                        fieldLabel:'Apellido Paterno',
+                        msgTarget:'side',
+                        allowBlank:false,
+                        vtype:'alpha',
+                        anchor:'95%'
+                    }]
+                },{
+                    xtype:'container',
+                    columnWidth:.50,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'textfield',
+                        name:'apellido_materno',
+                        fieldLabel:'Apellido Materno',
+                        msgTarget: 'side',
+                        allowBlank: true,
+                        vtype:'alpha',
+                        anchor:'95%'
+                    }]
+                }]
             },{
-                xtype:'textfield',
-                name:'apellido_paterno',
-                fieldLabel:'Apellido Paterno',
-                msgTarget:'side',
-                allowBlank:false,
-                vtype:'alpha',
-                anchor:'95%'
-            },{
-                xtype:'textfield',
-                name:'apellido_materno',
-                fieldLabel:'Apellido Materno',
-                msgTarget: 'side',
-                allowBlank: true,
-                vtype:'alpha',
-                anchor:'95%'
-            },{
-                xtype:'numberfield',
-                name:'telefono',
-                fieldLabel:'Telefono',
-                msgTarget:'side',
-                allowBlank: false,
-                allowDecimals:false,
-                //vtype:'alphanum',
-                minValue:0, //para evitar numeros negativos
-                hideTrigger: true,
-                keyNavEnabled: false,
-                mouseWheelEnabled: false,
-                anchor:'95%'
-            },{
-                xtype:'textfield',
-                name:'email',
-                fieldLabel:'Email',
-                msgTarget:'side',
-                allowBlank: false,
-                vtype: 'email',
-                anchor:'95%'
+                xtype:'container',
+                layout: 'column',
+                style:{
+                    paddingBottom:'20px'
+                },
+                items:[{
+                    xtype:'container',
+                    columnWidth:.30,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'numberfield',
+                        name:'telefono',
+                        fieldLabel:'Telefono',
+                        msgTarget:'side',
+                        allowBlank: false,
+                        allowDecimals:false,
+                        //vtype:'alphanum',
+                        minValue:0, //para evitar numeros negativos
+                        hideTrigger: true,
+                        keyNavEnabled: false,
+                        mouseWheelEnabled: false,
+                        anchor:'95%'
+                    }]
+                },{
+                    xtype:'container',
+                    columnWidth:.50,
+                    layout:'anchor',
+                    items:[{
+                        xtype:'textfield',
+                        name:'email',
+                        fieldLabel:'Email',
+                        msgTarget:'side',
+                        allowBlank: false,
+                        vtype: 'email',
+                        anchor:'95%'
+                    }]
+                }]
             }]
         }];
 
